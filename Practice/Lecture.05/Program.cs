30bc98114e617f47f2938bef8e5c9e5ee0537d8c
user <user@ECHTP-50-18>
2019-12-12 21:20:30 +0300

    Add sollution

---
+++ Practice/Lecture.05/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lecture._05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentHour = DateTime.Now.TimeOfDay.Hours;
+
+            if (currentHour >= 9 & currentHour < 12)
+            {
+                Console.WriteLine("Good morning, guys");
+            }
+            else if (currentHour>=12 & currentHour<15)
+            {
+                Console.WriteLine("Good day, guys");
+            }
+            else if (currentHour >= 15 & currentHour < 22)
+            {
+                Console.WriteLine("Good evening, guys");
+            }
+            else
+            {
+                Console.WriteLine("Good night, guys");
+            }
+        }
+    }
+}
