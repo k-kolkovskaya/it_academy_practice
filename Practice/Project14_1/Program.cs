10f107d453770995ac161a123e63624aebc60bbc
user <user@ECHTP-50-18>
2020-01-28 19:28:11 +0300

    Add 1 task without validation

---
+++ Practice/Project14_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Project14_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String text1 = "texttextdsffgdfg";
+            Console.WriteLine(text1.CutString());
+        }
+    }
+}
---
+++ Practice/Project14_1/StringExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project14_1
+{
+    static class StringExtension
+    {
+        public static string CutString (this String @string)
+        {
+            return @string.Substring(0, 5) + "..."; 
+        }
+    }
+}
