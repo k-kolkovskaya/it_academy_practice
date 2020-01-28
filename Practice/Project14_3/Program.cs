9803581194a41d817fa1abdfa5eab63251f0e3ef
user <user@ECHTP-50-18>
2020-01-28 19:52:47 +0300

    Add Task 3

---
+++ Practice/Project14_3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project14_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Neighbour K = new Neighbour("Some", 14, 3456788);
+            Neighbour A = new Neighbour("Something", 145, 34567883);
+            List<Neighbour> floorNeighbours = new List<Neighbour>() { K, A };
+        }
+    }
+}
